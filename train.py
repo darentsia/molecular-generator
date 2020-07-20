f448f6b58682505b751cae5d4615e2e329ae0c48
daridar <orgunova@gmail.com>
2020-07-20 14:09:12 +0000

    Apply black, isort

--- train.py
+++ train.py
@@ -1,16 +1,18 @@
 import argparse
 
+import numpy as np
 import pandas as pd
 import torch
+from sklearn.mixture import GaussianMixture
 from sklearn.model_selection import train_test_split
+from tensorboardX import SummaryWriter
 from torch.utils.data import DataLoader
+from tqdm import tqdm
 
 from src.dataset import SMILESDataset, create_vocab, make_collate_fn
 from src.training import configure_optimizer, configure_scheduler
 from src.transformer import Transformer
 from src.utils import seed_everything
-from tensorboardX import SummaryWriter
-from tqdm import tqdm
 
 
 def strip_molecule(molecule, end_tok):
@@ -30,23 +32,28 @@ def idx_to_char(output, train_dataset):
     return char_molecules
 
 
-def to_html(output):
+def to_markdown(output):
     df = pd.DataFrame({"Molecules": output})
-    html_str = df.to_markdown()
-    return html_str
+    markdown_str = df.to_markdown()
+    return markdown_str
 
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--data", type=str, help="Path to the data file.")
-    parser.add_argument("--lr", type=int, default=0.001, help="Learning rate.")
+    parser.add_argument("--lr", type=int, default=0.0001, help="Learning rate.")
     parser.add_argument(
         "--epochs", type=int, default=3000, help="Number of training epochs"
     )
-    parser.add_argument("--batch_size", type=int, default=32, help="Batch size.")
     parser.add_argument(
-        "--accumulation", type=int, default=1, help="Number of accumulation steps.",
+        "--batch_size", type=int, default=32, help="Batch size."
+    )
+    parser.add_argument(
+        "--accumulation",
+        type=int,
+        default=1,
+        help="Number of accumulation steps.",
     )
     parser.add_argument(
         "--warmup", type=int, default=3, help="Number of warmup epochs."
@@ -55,7 +62,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     seed_everything(13)
-    writer = SummaryWriter("runs/test_run_3")
+    writer = SummaryWriter("runs/test_run_logP")
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     # device = torch.device("cpu")
@@ -67,14 +74,27 @@ if __name__ == "__main__":
         print(torch.cuda.get_device_name(0))
         print("Memory Usage:")
         print(
-            "Allocated:", round(torch.cuda.memory_allocated(0) / 1024 ** 3, 1), "GB",
+            "Allocated:",
+            round(torch.cuda.memory_allocated(0) / 1024 ** 3, 1),
+            "GB",
         )
         print(
             "Cached: ", round(torch.cuda.memory_cached(0) / 1024 ** 3, 1), "GB",
         )
 
     df = pd.read_csv(args.data)
-    df = df.sample(100)
+
+    # Obtain mean, std logP for normalization
+    log_p = np.array(df.Kow.tolist())
+    mean_log_p = log_p.mean()
+    std_log_p = log_p.std()
+
+    print("Fitting GMM...")
+    gmm = GaussianMixture(n_components=3)
+    gmm.fit(log_p.reshape(-1, 1))
+    print("Finished fitting GMM.")
+
+    # df = df.sample(100)
     train, test_val = train_test_split(df, test_size=0.1)
     test, val = train_test_split(test_val, test_size=0.5)
 
@@ -83,7 +103,7 @@ if __name__ == "__main__":
     vocab = create_vocab(df)
     print("Vocab size: ", len(vocab))
 
-    train_dataset = SMILESDataset(train, vocab)
+    train_dataset = SMILESDataset(train, vocab, mean_log_p, std_log_p)
     print("With special tokens", train_dataset.vocab_size)
     print(train_dataset.char_to_idx)
     train_loader = DataLoader(
@@ -91,17 +111,17 @@ if __name__ == "__main__":
         batch_size=args.batch_size,
         collate_fn=make_collate_fn(padding_values=train_dataset.padding_values),
         shuffle=True,
-        drop_last=True
+        drop_last=True,
     )
 
-    val_dataset = SMILESDataset(val, vocab)
+    val_dataset = SMILESDataset(val, vocab, mean_log_p, std_log_p)
     val_loader = DataLoader(
         val_dataset,
         batch_size=args.batch_size,
         collate_fn=make_collate_fn(padding_values=train_dataset.padding_values),
     )
 
-    test_dataset = SMILESDataset(test, vocab)
+    test_dataset = SMILESDataset(test, vocab, mean_log_p, std_log_p)
     test_loader = DataLoader(
         test_dataset,
         batch_size=args.batch_size,
@@ -116,6 +136,7 @@ if __name__ == "__main__":
         dim_feedforward=1024,  # 1024
         dropout=0.1,
         num_positions=1024,
+        n_conditional_channels=1,
     )
     model.to(device)
 
@@ -123,12 +144,12 @@ if __name__ == "__main__":
     steps_per_epoch = len(train_dataset) / (args.batch_size * args.accumulation)
     scheduler = configure_scheduler(
         optimizer,
-        training_steps=(
-            args.epochs * steps_per_epoch
-        ),
-        warmup=args.warmup * steps_per_epoch
+        training_steps=(args.epochs * steps_per_epoch),
+        warmup=args.warmup * steps_per_epoch,
     )
     criterion = torch.nn.CrossEntropyLoss(reduction="none")
+
+    best_val_loss = 1000
     for epoch in range(args.epochs):
         model.train()
         epoch_loss = 0
@@ -141,7 +162,9 @@ if __name__ == "__main__":
             target_sequence_length = batch["seq_len"].to(device)
 
             output = model(
-                output_ids=smiles, target_sequence_length=target_sequence_length
+                output_ids=smiles,
+                target_sequence_length=target_sequence_length,
+                log_p=log_p,
             )
             loss = torch.mean(
                 torch.sum(
@@ -158,19 +181,25 @@ if __name__ == "__main__":
             loss.backward()
             optimizer.step()
             scheduler.step()
+
         print(f"Epoch {epoch} loss: {epoch_loss}")
         model.eval()
+        sample_log_p, _ = gmm.sample(4)
+        sample_log_p = (
+            torch.from_numpy(sample_log_p).squeeze(1).float().to(device)
+        )
         output = model.generate(
             batch_size=4,
             max_target_sequence_length=50,
             start_id=train_dataset.start_idx,
             device=device,
             mask_ids=(train_dataset.pad_idx, train_dataset.start_idx),
-            temperature=1.0
+            temperature=1.0,
+            log_p=sample_log_p,
         )
         generated = idx_to_char(output, train_dataset)
         print(generated)
-        writer.add_text("Generated_molecule", to_html(generated), epoch)
+        writer.add_text("Generated_molecule", to_markdown(generated), epoch)
         writer.add_scalar("train_loss", epoch_loss, epoch)
 
         # Validation
@@ -184,7 +213,9 @@ if __name__ == "__main__":
                 target_sequence_length = batch["seq_len"].to(device)
 
                 output = model(
-                    output_ids=smiles, target_sequence_length=target_sequence_length
+                    output_ids=smiles,
+                    target_sequence_length=target_sequence_length,
+                    log_p=log_p,
                 )
                 loss = torch.mean(
                     torch.sum(
@@ -200,16 +231,10 @@ if __name__ == "__main__":
                 val_loss += loss.item() / len(val_dataset) * len(smiles)
             print(f"Epoch {epoch} validation loss: {val_loss}")
             writer.add_scalar("val_loss", val_loss, epoch)
+            
+            # Model saving 
+            if val_loss < best_val_loss:
+                best_val_loss = val_loss
+                torch.save(model.state_dict(), "best_model.pt")
 
-    output = model.generate(
-        batch_size=4,
-        max_target_sequence_length=50,
-        start_id=train_dataset.start_idx,
-        device=device,
-        mask_ids=(train_dataset.pad_idx, train_dataset.start_idx),
-    )
-    print(idx_to_char(output, train_dataset))
-
-    import ipdb
-
-    ipdb.set_trace()
+    import ipdb; ipdb.set_trace()
